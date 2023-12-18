4fb727ca52416e874b38ee8e1d7c1d211a1b2063
Freeborger <ben.and.david.walker@gmail.com>
2023-12-18 09:08:00 -0700

    branch 2

---
+++ script.js
@@ -0,0 +1,27 @@
+//get card elements
+const headers = document.querySelectorAll('.card-header')
+
+const imgs = document.querySelectorAll('.card-img')
+
+const address = document.querySelectorAll('.card-address')
+
+
+//fetch and paste
+const url = 'https://iskarr.github.io/austindonovan.github.io/api/business.json'
+fetch(url)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        for (let i = 0; i < headers.length; i++) {
+
+            headers[i].textContent = jsObject.business[i].name
+
+            imgs[i].src = jsObject.business[i].imageurl
+
+            address[i].textContent = jsObject.business[i].address
+
+        }
+    })
+
+
+
